76e205389e7da7d3ca7b583cafe3bc72280df3c4
carlosoliveira397 <36646274+carlosoliveira397@users.noreply.github.com>
2024-05-24 14:01:35 -0300

    :construction: build config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+        --- Definir variáveis constantes de conexão
+        php.net pesquisar por PDO::_construct
+    */
+
+    define('MYSQL_HOST', 'localhost');
+    define('MYSQL_DATABASE', 'au25');
+    define('MYSQL_USER', 'root');
+    define('MYSQL_PASS', '');
+?>
